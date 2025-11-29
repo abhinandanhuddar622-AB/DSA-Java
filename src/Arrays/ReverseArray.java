74da1d660a7ea94c325d36aa1446831d04c1cea7
Abhinandan V Huddar <abhinandanhuddar622@gmail.com>
2025-11-29 18:35:04 +0530

    Added ReverseArray program

---
+++ src/Arrays/ReverseArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+        int start=0,end= arr.length-1;//there the 0 means first element(1)
+        // and -1 means last element(5)
+        while (start<end){  //1<5 ==true
+            int temp=arr[start]; //aar[start] stored in temp
+            arr[start]=arr[end]; //
+            arr[end]=temp;
+            start++; //start is incrementing
+            end--;   //end is decrementing
+        }
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+}
